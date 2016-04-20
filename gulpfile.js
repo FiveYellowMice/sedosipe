b0e3ba88083610a2977d0920b9718952dc06ced2
Kaizhe Huang <hkz85825915@gmail.com>
2016-04-20 16:55:03 +1000

    Add file change live build

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,51 @@
 const gulp = require("gulp");
 const liquid = require('gulp-liquid');
+const connect = require("gulp-connect");
 const fs = require("fs");
-const rimraf = require("rimraf");
+const del = require("del");
 const yaml = require("js-yaml");
 const child = require("child_process");
 
-gulp.task("build", function() {
-	rimraf.sync("_build/*");
-	gulp.src(["**/*.html", "**/*.js", "!bower_components/**", "!node_modules/**", "!_build/**", "!gulpfile.js"])
+var liquidFiles = ["**/*.html", "**/*.js", "!node_modules/**", "!_build/**", "!gulpfile.js"];
+
+function build(glob) {
+	gulp.src(glob)
 	.pipe(liquid({
 		locals: yaml.safeLoad(fs.readFileSync("strings.yaml", "utf-8"))
 	}))
 	.pipe(gulp.dest("./_build/"));
+}
+
+gulp.task("serve", ["rebuild"], function() {
+	gulp.watch(liquidFiles, function(event) {
+		console.log(`'${event.path}' has ${event.type}.`);
+		switch (event.type) {
+			case "added":
+			case "changed":
+				build(event.path);
+				break;
+			case "deleted":
+				del(event.path);
+				break;
+		}
+	});
+	gulp.watch("strings.yaml", ["rebuild"]);
+
+	connect.server({
+		root: "_build",
+		port: 8000
+	});
+});
+
+gulp.task("rebuild", function() {
+	del(["_build/**", "!_build", "!_build/bower_components/**"]);
+	build(liquidFiles);
 });
 
 gulp.task("bower", function(cb) {
-	try {
-		fs.mkdirSync("_build");
-		fs.symlinkSync("../bower_components", "./_build/bower_components");
-	} catch (e) {
-		if (e.code !== "EEXIST") throw e;
-	}
 	child.exec("bower update", function() {
 		cb();
 	});
 });
 
-gulp.task("default", ["build", "bower"]);
+gulp.task("default", ["rebuild", "bower"]);
